be03c99181b78187d79d6122f4a318b2fc33bb19
Mia Park <mkp5567@psu.edu>
2021-04-12 19:12:23 -0400

    apps.js change

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var appsRouter = require('./routes/apps');
+var dessertsRouter = require('./routes/desserts');
+var entreesRouter = require('./routes/entrees');
+var merchRouter = require('./routes/merch');
+var aboutusRouter = require('./routes/about-us');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/apps', appsRouter);
+app.use('/desserts', dessertsRouter);
+app.use('/entrees', entreesRouter);
+app.use('/merch', merchRouter);
+app.use('/about-us', aboutusRouter);
 
 module.exports = app;
