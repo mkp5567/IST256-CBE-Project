a01aa2ae44128b9c2571c3e952d12cab7f4edd15
Mia Park <mkp5567@psu.edu>
2021-04-25 15:19:55 -0400

    trying to make CRUD

---
+++ models/student.js
@@ -0,0 +1,13 @@
+//copied from zyBooks figure 11.8.7
+
+// models/student.js
+const db = require("../db");
+
+const Student = db.model("Student", {
+    name:      String,
+    gpa:       { type: Number, min: 0, max: 4 },
+    birthDate: { type: Date, default: Date.now },
+    interests: [ String ]
+});
+
+module.exports = Student;
