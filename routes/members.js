591a5f617d218363734dabdf5f5b4d36882f58ab
Mia Park <mkp5567@psu.edu>
2021-04-28 22:02:37 -0400

    html

--- routes/members.js
+++ routes/members.js
@@ -8,12 +8,8 @@ const Member = require("../models/member");
 router.get("/", memCnt1.findAll);
 router.post("/", memCnt1.create);
 
-<<<<<<< HEAD
-//router.put("/", memCnt1.Update);
-=======
 // router.put("/", memCnt1.Update);
 
->>>>>>> ddf2463e4a57fdb3de388b0dd84d552ecdc5e202
 router.delete("/:_id", memCnt1.delete);
 
 module.exports = router;
